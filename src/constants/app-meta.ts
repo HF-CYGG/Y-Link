5e88d7fc26675d69e065af3efcbe2992bc602b5d
夜喵cats <yyh1677696627@outlook.com>
2026-09-17 21:12:09 +0800

    chore(version): 更新应用版本至 v1.5.31.0

--- src/constants/app-meta.ts
+++ src/constants/app-meta.ts
@@ -1,5 +1,5 @@
 export const APP_META = {
-  version: 'v1.5.30.4',
+  version: 'v1.5.31.0',
   developer: 'by_夜喵cats',
   repositoryLabel: 'GitHub',
   repositoryUrl: 'https://github.com/HF-CYGG/Y-Link',
